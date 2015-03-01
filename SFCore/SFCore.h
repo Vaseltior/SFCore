ce4d0ed0a254d4596ce80af12658d3547c716c10
Samuel GRAU <samuel.grau@gmail.com>
2015-03-01 14:55:31 +0100

    Initial Commit

---
+++ SFCore/SFCore.h
@@ -0,0 +1,19 @@
+//
+//  SFCore.h
+//  SFCore
+//
+//  Created by Samuel Grau on 01/03/2015.
+//  Copyright (c) 2015 Samuel Grau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFCore.
+FOUNDATION_EXPORT double SFCoreVersionNumber;
+
+//! Project version string for SFCore.
+FOUNDATION_EXPORT const unsigned char SFCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFCore/PublicHeader.h>
+
+
