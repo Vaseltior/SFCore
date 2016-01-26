420fa0a1e09151eef938deee1325aa23bc6f9541
Samuel GRAU <samuel.grau@gmail.com>
2016-01-26 20:22:05 +0100

    [*] - Updating indentation

--- SFCore/sources/_extensions/String+SFCharacters.swift
+++ SFCore/sources/_extensions/String+SFCharacters.swift
@@ -32,28 +32,28 @@
 import Foundation
 
 extension String {
-    
-    ///
-    /// Returns the representation of the character at position `i`
-    /// in the string
-    ///
-    /// :param: i the index of the wanted character
-    ///
-    /// :returns: The string describing the character at index `i`.
-    ///
-    func getCharacter(i: Int) -> Character {
-        return self[advance(self.startIndex, i)]
-    }
-    
-    ///
-    /// Returns the representation of the character at position `i`
-    /// in the string
-    ///
-    /// :param: i the index of the wanted character
-    ///
-    /// :returns: The string describing the character at index `i`.
-    ///
-    subscript (i: Int) -> String {
-        return String(self.getCharacter(i) as Character)
-    }
+
+  ///
+  /// Returns the representation of the character at position `i`
+  /// in the string
+  ///
+  /// :param: i the index of the wanted character
+  ///
+  /// :returns: The string describing the character at index `i`.
+  ///
+  func getCharacter(i: Int) -> Character {
+    return self[advance(self.startIndex, i)]
+  }
+
+  ///
+  /// Returns the representation of the character at position `i`
+  /// in the string
+  ///
+  /// :param: i the index of the wanted character
+  ///
+  /// :returns: The string describing the character at index `i`.
+  ///
+  subscript (i: Int) -> String {
+    return String(self.getCharacter(i) as Character)
+  }
 }
