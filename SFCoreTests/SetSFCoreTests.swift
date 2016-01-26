c8a3385287a6764912861cc3a93edf7789c0d4a1
Samuel GRAU <samuel.grau@gmail.com>
2016-01-26 20:05:36 +0100

    [+] - Adding the map function on sets + unit tests

---
+++ SFCoreTests/SetSFCoreTests.swift
@@ -0,0 +1,37 @@
+//
+//  SetSFCoreTests.swift
+//  SFCore
+//
+//  Created by Samuel Grau on 26/01/2016.
+//  Copyright © 2016 Samuel Grau. All rights reserved.
+//
+
+import XCTest
+
+class SetSFCoreTests: XCTestCase {
+
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func testMapCardinalityOnSet() {
+    let a: Set<Int> = [4, 6, 1, 5]
+    let b = a.map { $0 + 1}
+    XCTAssertTrue(a.count == b.count, "The cardinality of the initial and the resulting set should be equal")
+  }
+
+  func testMapIdentityOnSet() {
+    let a: Set<Int> = [4, 6, 1, 5]
+    let b = a.map { $0 }
+    a.forEach {
+      XCTAssertTrue(b.contains($0), "Every element of a should exist in b")
+    }
+
+  }
+}
