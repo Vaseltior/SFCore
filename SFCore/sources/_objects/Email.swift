6658566790832cbce0f4d6f4a7ab53ba89f51f16
Samuel GRAU <samuel.grau@gmail.com>
2016-01-30 22:47:06 +0100

    ✨ Adding Email class and Observable property class

---
+++ SFCore/sources/_objects/Email.swift
@@ -0,0 +1,74 @@
+//
+//  Email.swift
+//  SFCore
+//
+//  Created by Samuel Grau on 30/01/2016.
+//  Copyright © 2016 Samuel Grau. All rights reserved.
+//
+
+import Foundation
+
+/**
+ *  @author Samuel GRAU, 16-01-30 22:01:36
+ *
+ *  A simple Email class that is able to validate correctly an Email
+ */
+public class Email: CustomStringConvertible {
+  var emailString: String? = nil
+  
+  // MARK: - Initialization
+  
+  /**
+  Creates an Email object with an email string
+  
+  - parameter email: the email string representation
+  
+  - returns: an Email object
+  */
+  public init(email: String) {
+    self.emailString = email.lowercaseString
+  }
+  
+  // MARK: - Email validation functions
+  
+  /**
+  Tells if the current email representation is valid or not
+  
+  - returns: true if the email is valid, otherwise false
+  */
+  public func isValidEmail() -> Bool {
+    guard let emailString = self.emailString else {
+      return false
+    }
+    
+    let emailRegEx =
+    "(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}" +
+      "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
+      "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-" +
+      "z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5" +
+      "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
+      "9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
+    "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+    
+    let regExPredicate = NSPredicate(format:"%@ MATCHES %@", emailString, emailRegEx)
+    return regExPredicate.evaluateWithObject(self)
+  }
+  
+  /**
+   Tells if the given string is a valid email representation
+   
+   - parameter emailString: the email string representation
+   
+   - returns: true if the email is valid, otherwise false
+   */
+  public static func isValidEmailString(emailString: String) -> Bool {
+    return Email(email: emailString).isValidEmail()
+  }
+  
+  // MARK: - CustomStringConvertible
+  
+  /// A textual representation of `self`.
+  public var description: String {
+    return (self.emailString ?? "")
+  }
+}
---
+++ SFCore/sources/_observation/ObservablePropertyProtocol.swift
@@ -0,0 +1,84 @@
+//
+//  ObservablePropertyProtocol.swift
+//  SFCore
+//
+//  Created by Samuel Grau on 30/01/2016.
+//  Copyright © 2016 Samuel Grau. All rights reserved.
+//
+
+import Foundation
+
+/**
+ *  @author Samuel GRAU, 16-01-30 22:01:36
+ *
+ *  Implements an observable property type. Kind of KVO in Swift.
+ */
+public protocol ObservableProperty {
+  /// The property type that is observed
+  typealias PropertyType
+  /// Property changed
+  var propertyChanged: Event<(PropertyType, Any)> { get }
+}
+
+/**
+ *  @author Samuel GRAU, 16-01-30 22:01:41
+ *
+ *  Declare a protocol that is disposable. Part of the observation mechanism
+ */
+public protocol Disposable {
+  /**
+   Should dispose the disposable implementer
+   */
+  func dispose()
+}
+
+/**
+ An Event class representation
+ */
+public class Event<T> {
+  
+  public typealias EventHandler = T -> ()
+  
+  private var eventHandlers = [Invocable]()
+  
+  public func raise(data: T) {
+    self.eventHandlers.forEach {
+      $0.invoke(data)
+    }
+  }
+  
+  public func addHandler<U: AnyObject>(target: U, handler: (U) -> EventHandler) -> Disposable {
+    let wrapper = EventHandlerWrapper(target: target, handler: handler, event: self)
+    eventHandlers.append(wrapper)
+    return wrapper
+  }
+}
+
+/// Protocol for things that are invacable
+private protocol Invocable: class {
+  func invoke(data: Any)
+}
+
+/// A wrapper for
+private class EventHandlerWrapper<T: AnyObject, U>: Invocable, Disposable {
+  weak var target: T?
+  let handler: T -> U -> ()
+  let event: Event<U>
+  
+  init(target: T?, handler: T -> U -> (), event: Event<U>) {
+    self.target = target
+    self.handler = handler
+    self.event = event
+  }
+  
+  func invoke(data: Any) -> () {
+    if let t = target {
+      handler(t)(data as! U)
+    }
+  }
+  
+  func dispose() {
+    event.eventHandlers =
+      event.eventHandlers.filter { $0 !== self }
+  }
+}
