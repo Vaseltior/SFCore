f97867aefb887217856c42b458fbb9e9eca3eb0c
Samuel GRAU <samuel.grau@gmail.com>
2016-03-22 14:36:43 +0100

    ⚙️ Swift 2.2

--- SFCore/sources/_settings/Settings+Texting.swift
+++ SFCore/sources/_settings/Settings+Texting.swift
@@ -24,3 +24,31 @@ extension SettingsHelper {
     return try SettingsHelper.buildAndOpenURL(URLString)
   }
 }
+
+/**
+You can't detect the simulator directly, but you can detect iOS on a desktop architecture like follows
+
+#if (arch(i386) || arch(x86_64)) && os(iOS)
+...
+#endif
+Clearly this is false on a device, but it returns true for the iOS Simulator, as specified in the documentation:
+
+The arch(i386) build configuration returns true when code is compiled for the 32–bit iOS simulator.
+If you are developing for a simulator other than iOS, you can simply vary the os parameter: e.g.
+
+Detect the watchOS simulator
+
+#if (arch(i386) || arch(x86_64)) && os(watchOS)
+...
+#endif
+Detect the tvOS simulator
+
+#if (arch(i386) || arch(x86_64)) && os(tvOS)
+...
+#endif
+Or, even, detect any simulator
+
+#if (arch(i386) || arch(x86_64)) && (os(iOS) || os(watchOS) || os(tvOS))
+...
+#endif
+*/
